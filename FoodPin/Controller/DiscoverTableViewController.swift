94eac3d3083c2ad549a709ec877dbbc3bb79dfde
Roger Florat <rflorat@cidiverte.it>
2018-04-18 10:44:33 +0200

    FoodPin - Restyling discover table

--- FoodPin/Controller/DiscoverTableViewController.swift
+++ FoodPin/Controller/DiscoverTableViewController.swift
@@ -24,6 +24,7 @@ class DiscoverTableViewController: UITableViewController {
         fetchRecordsFromCloud()
         
         showPullRefreshControl()
+            
     }
     
     // MARK: - Activity Indicator
@@ -65,13 +66,12 @@ class DiscoverTableViewController: UITableViewController {
         let cloudContainer = CKContainer.default()
         let publicDatabase = cloudContainer.publicCloudDatabase
         let predicate = NSPredicate(value: true)
-        
         let query = CKQuery(recordType: "Restaurant", predicate: predicate)
         query.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
         
         // Create the query operation with the query
         let queryOperation = CKQueryOperation(query: query)
-        queryOperation.desiredKeys = ["name"]
+        queryOperation.desiredKeys = ["name", "type", "location"]
         queryOperation.queuePriority = .veryHigh
         queryOperation.resultsLimit = 50
         queryOperation.recordFetchedBlock = { (record) -> Void in
@@ -83,13 +83,16 @@ class DiscoverTableViewController: UITableViewController {
                 print("Failed to get data from iCloud - \(error.localizedDescription)")
                 
                 self.hideActivityIndicator()
+                
                 return
             }
             print("Successfully retrieve the data from iCloud")
             
             OperationQueue.main.addOperation {
                 self.spinner.stopAnimating()
+                
                 self.tableView.reloadData()
+                
                 self.hideActivityIndicator()
             }
         }
@@ -111,21 +114,23 @@ class DiscoverTableViewController: UITableViewController {
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! RestaurantTableViewCell
         
         // Configure the cell...
         let restaurant = restaurants[indexPath.row]
-        cell.textLabel?.text = restaurant.object(forKey: "name") as? String
+        cell.nameLabel?.text = restaurant.object(forKey: "name") as? String
+        cell.typeLabel?.text = restaurant.object(forKey: "type") as? String
+        cell.locationLabel?.text = restaurant.object(forKey: "location") as? String
         
         // Set the default image
-        cell.imageView?.image = UIImage(named: "photoalbum")
+        cell.thumbnailImageView?.image = UIImage(named: "photo")
         
         // Check if the image is stored in cache
         if let imageFileURL = imageCache.object(forKey: restaurant.recordID) {
             // Fetch image from cache
             print("Get image from cache")
             if let imageData = try? Data.init(contentsOf: imageFileURL as URL) {
-                cell.imageView?.image = UIImage(data: imageData)
+                cell.thumbnailImageView?.image = UIImage(data: imageData)
             }
         } else {
             // Fetch Image from Cloud in background
@@ -134,8 +139,7 @@ class DiscoverTableViewController: UITableViewController {
             fetchRecordsImageOperation.desiredKeys = ["image"]
             fetchRecordsImageOperation.queuePriority = .veryHigh
             
-            fetchRecordsImageOperation.perRecordCompletionBlock = { (record, recordID,
-                error) -> Void in
+            fetchRecordsImageOperation.perRecordCompletionBlock = { (record, recordID, error) -> Void in
                 if let error = error {
                     print("Failed to get restaurant image: \(error.localizedDescription)")
                     return
@@ -144,9 +148,8 @@ class DiscoverTableViewController: UITableViewController {
                     OperationQueue.main.addOperation() {
                         if let image = restaurantRecord.object(forKey: "image") {
                             let imageAsset = image as! CKAsset
-                            if let imageData = try? Data.init(contentsOf:
-                                imageAsset.fileURL) {
-                                cell.imageView?.image = UIImage(data: imageData)
+                            if let imageData = try? Data.init(contentsOf: imageAsset.fileURL) {
+                                cell.thumbnailImageView?.image = UIImage(data: imageData)
                             }
                             // Add the image URL to cache
                             self.imageCache.setObject(imageAsset.fileURL as NSURL, forKey: restaurant.recordID)
